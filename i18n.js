df38ad65dcb3d1103ef0f9e62caac32e7e8e133b
Boxvi <hermosokawai@gmail.com>
2025-03-12 10:33:01 -0500

    cambio completo

--- i18n.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    locales: ['es', 'en', 'fr'],
-    defaultLocale: 'es',
-    pages: {
-        '*': ['common', 'zod'] // Namespaces that you want to import per page (we stick to one namespace for all the application in this tutorial)
-    },
-    defaultNS: 'common'
-};
--- localesSync.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    primaryLanguage: 'es',
-    secondaryLanguages: ['en'],
-    tertiaryLanguages : ['fr'],
-    localesFolder: './public/locales',
-    overridePluralRules: (pluralResolver) => pluralResolver.addRule('en', pluralResolver.getRule('es'),
-        pluralResolver.getRule('fr') ), // This is available only when using config file
-    spaces: 2
-};
