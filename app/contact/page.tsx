58ed0750f227456faaeee07d45dcc9d9a4162556
Boxvi <hermosokawai@gmail.com>
2025-03-13 16:15:20 -0500

    pagina principal v1

---
+++ app/contact/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Github, Mail, Phone, Twitter, Wifi } from "lucide-react";
+import Link from "next/link";
+import { Navigation } from "../components/nav";
+import { Card } from "../components/card";
+
+const socials = [
+	{
+		icon: <Phone size={20} />,
+		href: "https://wa.me/593996474990?text=Hola%20Boris,%20%C2%BFc%C3%B3mo%20te%20encuentras%20el%20d%C3%ADa%20de%20hoy?",
+		label: "WhatsApp",
+		handle: "Boxvi Soft",
+	},
+	{
+		icon: <Mail size={20} />,
+		href: "mailto:hola@boxvisoft.com",
+		label: "Email",
+		handle: "hola@boxvisoft.com",
+	},
+	{
+		icon: <Github size={20} />,
+		href: "https://github.com/Boxvi",
+		label: "Github",
+		handle: "Boxvi",
+	},
+	{
+		icon: <Wifi size={20} />,
+		href: "https://portafolio.boxvisoft.com",
+		label: "Portafolio",
+		handle: "portafolio.boxvisoft.com",
+	},
+];
+
+export default function Example() {
+	return (
+		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+			<Navigation />
+			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+					{socials.map((s) => (
+						<Card>
+							<Link
+								href={s.href}
+								target="_blank"
+								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+							>
+								<span
+									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+									aria-hidden="true"
+								/>
+								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+									{s.icon}
+								</span>{" "}
+								<div className="z-10 flex flex-col items-center">
+									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
+										{s.handle}
+									</span>
+									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+										{s.label}
+									</span>
+								</div>
+							</Link>
+						</Card>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ app/layout.tsx
@@ -0,0 +1,76 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Boxvi Soft",
+    template: "%s | Boxvi Soft",
+  },
+  description: "Descubre Boxvi Soft, desarrollo web, software y soluciones digitales para tu negocio.",
+  openGraph: {
+    title: "boxvisoft.com",
+    description:
+      "Descubre Boxvi Soft, desarrollo web, software y soluciones digitales para tu negocio.",
+    url: "https://www.boxvisoft.com",
+    siteName: "boxvisoft.com",
+    images: [
+      {
+        url: "https://www.boxvisoft.com/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "BoxviSoft",
+    card: "summary_large_image",
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
